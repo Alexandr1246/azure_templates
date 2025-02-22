4bc0258a5c77b4f3502830ea68ce98945b4eb6b3
Alexandr1246 <alex.sh1246@gmail.com>
2025-02-22 23:14:10 +0200

    upgrade script for luching network and balacer

--- Load_Balancer/loader_script.sh
+++ Load_Balancer/loader_script.sh
@@ -5,12 +5,16 @@ RESOURCE_GROUP="Student"
 # Region for resource group
 LOCATION="eastus"
 # The path to the template file
-TEMPLATE_FILE="network.json"
-
+TEMPLATE_FILE_NETWORK="network.json"
+TEMPLATE_FILE_BALANCER="load_balancer.json"
 # Creating a resource group
 echo "Creating a resource group $RESOURCE_GROUP in the region $LOCATION..."
 az group create --name "$RESOURCE_GROUP" --location "$LOCATION"
 
 # Deploy the template to the created resource group
-echo "Deploying a template from a file $TEMPLATE_FILE resource group $RESOURCE_GROUP..."
-az deployment group create --resource-group "$RESOURCE_GROUP" --template-file "$TEMPLATE_FILE"
+echo "Deploying a template from a file $TEMPLATE_FILE_NETWORK resource group $RESOURCE_GROUP..."
+az deployment group create --resource-group "$RESOURCE_GROUP" --template-file "$TEMPLATE_FILE_NETWORK"
+
+echo "Deploying a template from a file $TEMPLATE_FILE_BALANCER resource group $RESOURCE_GROUP..."
+az deployment group create --resource-group "$RESOURCE_GROUP" --template-file "$TEMPLATE_FILE_BALANCER"
+
